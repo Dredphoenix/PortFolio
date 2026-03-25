d3b9ea0fd9ed5013387dc2cfaea14220851583fb
Dredphoenix <vishwabalak98@gmail.com>
2026-03-25 14:42:38 +0530

    updated Page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,32 +9,17 @@ import Services from "@/components/sections/Services";
 import Contact from "@/components/sections/Contact";
 import ProcessSection from "@/components/sections/ProcessSection";
 
-import { useState } from "react";
-import Navbar from "@/components/layouts/Navbar";
-
 export default function Home() {
-    const [wipeActive, setWipeActive] = useState(false);
   return (
-      <main className="bg-black text-white overflow-x-hidden">
-
-      <Navbar setWipeActive={setWipeActive} />
-
-      {/* CONTENT WRAPPER */}
-      <div
-        className={`transition-transform duration-500 ${
-          wipeActive ? "-translate-x-10 opacity-80" : "translate-x-0 opacity-100"
-        }`}
-      >
-        <Hero />
-        <ProcessSection/>
-        <About />
-        <Projects />
-        <Skills />
-        <Experience />
-        <Services />
-        <Contact />
-      </div>
-
+    <main className="bg-black text-white overflow-x-hidden">
+      <Hero />
+      <ProcessSection />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Services />
+      <Contact />
     </main>
   );
 }
